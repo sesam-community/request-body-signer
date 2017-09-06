0d755d5d1b8ac971ad9ddb79f5c7b3852ccf0c21
Ashkan Vahidishams <ashkan.vahidishams@bouvet.no>
2017-09-06 16:44:42 +0200

    got it working!

--- service/index.js
+++ service/index.js
@@ -3,6 +3,7 @@ var httpProxy = require('http-proxy');
 var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
 var url = require('url');
+var textBody = require("body");
 
 var issuer = process.env.JWT_ISSUER;
 var expiry = parseInt(process.env.JWT_EXPIRY || 300); // default is 5 minutes
@@ -12,21 +13,27 @@ var target = process.env.TARGET;
 
 var proxy = httpProxy.createProxyServer({});
 
-http.createServer(function (req, res) {
+proxy.on('proxyReq', function (proxyReq, req) {
     var hash = crypto.createHash(algoritm);
-    req.on('data', function (data) {
-        hash.update(data);
-    });
-    req.on('end', function () {
-        var payload = {algoritm: hash.digest('hex'), 'iss': issuer, 'exp': (new Date).getTime() + (expiry * 1000)};
-        myToken = jwt.sign(payload, signingKey);
-        req.headers["Authorization"] = 'bearer ' + myToken;
+    console.log("Body: " + req.body);
+    if (req.body) {
+        hash.update(req.body);
+    }
+    var payload = {'iss': issuer, 'exp': (new Date).getTime() + (expiry * 1000)};
+    payload[algoritm] = hash.digest('hex');
+    myToken = jwt.sign(payload, signingKey);
+    console.log(myToken);
+    proxyReq.setHeader('Authorization', 'Bearer ' + myToken);
+    proxyReq.write(req.body);
+});
 
+http.createServer(function (req, res) {
+    textBody(req, function (err, body) {
+        req.body = body;
         proxy.web(req, res, {
             target: target || req.url
         });
     });
-
 }).listen(8080);
 
 console.log("listening on port 8080");
