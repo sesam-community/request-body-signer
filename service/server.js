a728ea4962485ee1a28dd8d439ca10f976554f3f
Ashkan Vahidishams <ashkan.vahidishams@bouvet.no>
2018-03-05 14:31:30 +0100

    changing some parameter names and adding algorithm to jwt header

--- service/server.js
+++ service/server.js
@@ -3,7 +3,6 @@ var httpProxy = require('http-proxy');
 var crypto = require('crypto');
 var jwt = require('jsonwebtoken');
 var url = require('url');
-var textBody = require("body");
 
 var issuer = process.env.JWT_ISSUER;
 var expiry = parseInt(process.env.JWT_EXPIRY || 300); // default is 5 minutes
@@ -20,8 +19,9 @@ proxy.on('proxyReq', function (proxyReq, req) {
         hash.update(req.body);
     }
     var payload = {'iss': issuer, 'exp': (new Date).getTime() + (expiry * 1000)};
-    payload[algoritm] = hash.digest('hex');
-    myToken = jwt.sign(payload, signingKey);
+    payload['digest'] = hash.digest('hex');
+    myToken = jwt.sign(payload, signingKey, {"header": {"digestAlgorithm": algoritm}});
+
     console.log(myToken);
     proxyReq.setHeader('Authorization', 'Bearer ' + myToken);
     proxyReq.write(req.body);
